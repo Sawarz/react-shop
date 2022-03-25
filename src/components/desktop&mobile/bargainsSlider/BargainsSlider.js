2855b5dd73faaa520973f56a96d438495e84529d
unknown <sawarz22@gmail.com>
2022-03-25 18:23:53 +0100

    Added changing slides

--- src/components/desktop&mobile/bargainsSlider/BargainsSlider.js
+++ src/components/desktop&mobile/bargainsSlider/BargainsSlider.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import { React, useState } from 'react'
 import styles from './styles.module.css';
 import data from '../../../assets/bargains/bargains.json'
+import SliderButton from './sliderButton/SliderButton';
 
 export default function BargainsSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const bargains = data.bargains;
   let imgs = [];
+
+  function handleClick(direction) {
+    if (direction === "left") {
+      if (currentSlide === 0)
+        setCurrentSlide(data.bargains.length - 1);
+      else {
+        setCurrentSlide(currentSlide - 1);
+      }
+    }
+    else {
+      if (currentSlide === data.bargains.length - 1)
+        setCurrentSlide(0);
+      else {
+        setCurrentSlide(currentSlide + 1);
+      }
+    }
+    console.log(currentSlide)
+  }
+
   for (let i = 0; i < data.bargains.length; i++){
     console.log(bargains[i].src)
     imgs[i] = require(`../../../assets/bargains/${bargains[i].src}`);
   }
   return (
-    <div className={styles.bargainsSlider}>{imgs.map((img) => {
-      return (
-        <img src={img} width="250px" ></img>
-      )
-    })}</div>
+    <>
+      <div className={styles.bargainsSlider}>
+        <SliderButton direction="left" onClick={handleClick}></SliderButton>
+        <img src={imgs[currentSlide]} width="200px" ></img>
+        <SliderButton direction="right" onClick={handleClick}></SliderButton>
+      </div>
+    </>
   )
 }
