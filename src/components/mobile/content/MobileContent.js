405e6e18c6faa17a6271f15e751cd873f832c116
unknown <sawarz22@gmail.com>
2022-03-30 18:22:57 +0200

    Added Products to main content

--- src/components/mobile/content/MobileContent.js
+++ src/components/mobile/content/MobileContent.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './styles.module.css'
 import BargainsSlider from '../../desktop&mobile/bargainsSlider/BargainsSlider'
 import Categories from '../categories/Categories'
+import Products from '../products/Products'
 
 export default function MobileContent() {
   return (
     <div className={styles.mobileContent}>
       <BargainsSlider></BargainsSlider>
       <Categories></Categories>
+      <Products></Products>
     </div>
   )
 }
