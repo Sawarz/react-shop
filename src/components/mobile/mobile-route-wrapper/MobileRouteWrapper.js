b3ab54905256fc538b64062de9f17f52849886c0
unknown <sawarz22@gmail.com>
2022-03-31 15:43:07 +0200

    Deleted unused component

--- src/components/mobile/mobile-route-wrapper/MobileRouteWrapper.js
+++ src/components/mobile/mobile-route-wrapper/MobileRouteWrapper.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import MobileNavbar from '../navbar/MobileNavbar'
-import MobileContent from '../content/MobileContent'
 import Footer from '../../desktop&mobile/footer/Footer'
 
 export default function MobileRouteWrapper(props) {
-  return (
+    return (
     <div className='mobile-page'>
           <MobileNavbar></MobileNavbar>     
           <div className='page-content'>
