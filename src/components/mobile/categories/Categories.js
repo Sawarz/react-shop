f8fe6c1d0c14225f97090a4f29cbb4cdc8844f2b
unknown <sawarz22@gmail.com>
2022-04-01 19:27:40 +0200

    Added routing to SeeAll

--- src/components/mobile/categories/Categories.js
+++ src/components/mobile/categories/Categories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import data from '../../../assets/categories/categories.json'
 import styles from './styles.module.css'
 import Category from './category/Category'
@@ -9,7 +10,7 @@ export default function Categories() {
     <div className={styles.categoriesWrapper}>
         <div className={styles.categoriesTitle}>
             <h1>Top Categories</h1>
-            <div className={styles.seeAll}>See All</div>
+            <Link className={styles.seeAll} to='/products'>See All</Link>
         </div>
         <div className={styles.categories}>
             {categories.map((category, i) => {
