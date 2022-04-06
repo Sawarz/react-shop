27374ee50ecc5562c24fadc7671255da2318220e
unknown <sawarz22@gmail.com>
2022-04-06 18:49:32 +0200

    Added DesktopRouteWrapper

---
+++ src/components/desktop/desktop-route-wrapper/DesktopRouteWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../navbar/navbar/Navbar'
+import Footer from '../../desktop&mobile/footer/Footer'
+
+export default function DesktopRouteWrapper(props) {
+  return (
+    <div className='desktop-page'>
+          <Navbar></Navbar>     
+          <div className='desktop-page-content'>
+            {props.component}
+          </div>
+            <Footer></Footer>
+        </div>
+  )
+}
