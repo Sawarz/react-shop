54adc18cb229d6ec8837c5f73d2b5b1f861567c3
unknown <sawarz22@gmail.com>
2022-04-06 18:50:32 +0200

    Added Contact page to mobile

--- src/components/mobile/contact/Contact.js
+++ src/components/mobile/contact/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import styles from './styles.module.css'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className={styles.contact}>
+      <h1>Feel free to contact us!</h1>
+    </div>
   )
 }
