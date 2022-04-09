01c8bf434c2f9ac78a564313daf3f7726e95f8c9
unknown <sawarz22@gmail.com>
2022-04-09 14:14:55 +0200

    Add working form

--- src/components/desktop/contact/Contact.js
+++ src/components/desktop/contact/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
+import ContactForm from './ContactForm'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Contact() {
-  
   return (
     <div className={styles.contact}>
       <div className={styles.contactHeader}>
@@ -10,10 +12,19 @@ export default function Contact() {
         <p>If you have any questions or remarks about our products, just write a message!</p>
       </div>
       <div className={styles.contactInfo}>
-        <div className={styles.contactInfoElement}>221b Baker Street, London</div>
-        <div className={styles.contactInfoElement}>+123 456 789</div>
-        <div className={styles.contactInfoElement}>contact@react-shop.com</div>
+        <div className={styles.contactInfoElement}>
+          <FontAwesomeIcon icon={faLocationDot} />
+          <p>221B Baker Street, London</p>
+        </div>
+        <div className={styles.contactInfoElement}>
+          <FontAwesomeIcon icon={faPhone} />
+          <p>+123 456 789</p>
+        </div>
+        <div className={styles.contactInfoElement}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          contact@react-shop.com</div>
       </div>
+      <ContactForm></ContactForm>
     </div>
   )
 }
--- src/components/desktop/contact/ContactForm.js
+++ src/components/desktop/contact/ContactForm.js
@@ -1,32 +1,73 @@
 import React from 'react'
+import formStyles from './formStyles.module.css'
 import { useFormik } from 'formik'
 
 export default function ContactForm() {
-  const SignupForm = () => {
+  console.log(formStyles.input);
     const formik = useFormik({
       initialValues: {
+        name: '',
         email: '',
+        phone: '',
+        message: ''
       },
       onSubmit: values => {
         alert(JSON.stringify(values, null, 2));
       },
     });
     return (
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <button type="submit">Submit</button>
+      <form className={formStyles.form} onSubmit={formik.handleSubmit}>
+        <div className={formStyles.inputsWrapper}>
+          <div className={formStyles.formColumn}>
+            <div className={formStyles.inputLabelWrapper}>
+              <label htmlFor="name">Your Name</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                className={formStyles.input}
+                onChange={formik.handleChange}
+                value={formik.values.name}
+              />
+            </div>
+            <div className={formStyles.inputLabelWrapper}>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className={formStyles.input}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+            </div>
+            <div className={formStyles.inputLabelWrapper}>
+              <label htmlFor="phone">Phone</label>
+              <input
+                id="phone"
+                name="phone"
+                type="text"
+                className={formStyles.input}
+                onChange={formik.handleChange}
+                value={formik.values.phone}
+                />
+            </div>
+          </div>
+          <div className={formStyles.formColumn}>
+            <div className={formStyles.inputLabelWrapper}>
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                type="text"
+                className={`${formStyles.input} ${formStyles.textarea}`}
+                onChange={formik.handleChange}
+                value={formik.values.message}
+              />
+            </div>
+          </div>
+        </div>
+        <button className={formStyles.submitButton} type="submit">Send message</button>
       </form>
     );
-  };
-  
-  return (
-    <div>ContactForm</div>
-  )
-}
+  }
