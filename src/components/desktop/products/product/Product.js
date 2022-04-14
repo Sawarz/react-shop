5097361d814da9dd073182952a45c361cfaac6f9
unknown <sawarz22@gmail.com>
2022-04-14 16:36:05 +0200

    Add product component

--- src/components/desktop/products/product/Product.js
+++ src/components/desktop/products/product/Product.js
@@ -1,21 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
 import styles from './styles.module.css'
+import data from '../../../../assets/products/products.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as starEmpty} from '@fortawesome/free-regular-svg-icons'
+import { faStar as starFilled } from '@fortawesome/free-solid-svg-icons'
 
-export default function Product(props) {
-    let id = props.product.id;
-    let imgSrc = require(`../../../../assets/products/${props.product.src}`);
-    console.log(props.product);
-    function goToProductPage() {
-        console.log("object");
-    }
+export default function Product() {
+    let product;
+    
+    let stars = [starEmpty, starEmpty, starEmpty, starEmpty, starEmpty];
+    let productURL = useParams().product;
+    data.products.forEach((currentProduct) => {
+        if (currentProduct.name === productURL) {
+            product = currentProduct;
+        }
+    })
+    let imgSrc = require(`../../../../assets/products/${product.name}.png`);
+
+    (function determineStars() {
+        console.log(product.stars);
+        switch (product.stars) {
+            case 5:
+                stars[4] = starFilled;
+                console.log("5")
+            case 4:
+                console.log("es")
+                stars[3] = starFilled;
+            case 3:
+                stars[2] = starFilled;
+            case 2:
+                stars[1] = starFilled;
+            case 1:
+                stars[0] = starFilled;
+        }
+    })();
+
+    useEffect(() => {
+       console.log(product); 
+    },[])
   return (
-      <Link className={styles.product} to={`/products/${props.product.name}`}>
-          <div>Product</div>
-          <div className={styles.imgWrapper}>
-            <img src={imgSrc}></img>
+      <div className={styles.product}>
+          <div className={styles.mainInfo}>
+              <div className={styles.imgWrapper}>
+                <img className={styles.productImg} src={imgSrc} alt={product.name}></img>
+              </div>
+              <div className={styles.shortInfo}>
+                  <div className={styles.infoTitle}>
+                      {product.name}
+                  </div>
+                  <div className={styles.category}>
+                      {product.category}
+                  </div>
+                  <div className={styles.infoReviews}>
+                      <FontAwesomeIcon icon={stars[0]}></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={stars[1]}></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={stars[2]}></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={stars[3]}></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={stars[4]}></FontAwesomeIcon>
+                  </div>
+                  <div className={styles.description}>
+                      {product.description}
+                  </div>
+                  <div className={styles.infoReviews}></div>
+                  <div className={styles.mainAdvantages}></div>
+              </div>
           </div>
-          <div>{props.product.price}</div>
-    </Link>
+    </div>
   )
 }
---
+++ src/components/desktop/products/productInfo/ProductInfo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css'
+
+export default function ProductInfo(props) {
+    let imgSrc = require(`../../../../assets/products/${props.product.src}`);
+    console.log(props.product);
+  
+  return (
+      <Link className={styles.product} to={`/products/${props.product.name}`}>
+          <div>Product</div>
+          <div className={styles.imgWrapper}>
+            <img src={imgSrc} alt={props.product.name}></img>
+          </div>
+          <div>{props.product.price}</div>
+    </Link>
+  )
+}
