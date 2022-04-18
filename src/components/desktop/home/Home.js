553b40c4f5b9f509f2e372e94c3bdd7df9bd1899
Sawarz <sawarz22@gmail.com>
2022-04-18 16:09:24 +0200

    Add matrix animation

--- src/components/desktop/home/Home.js
+++ src/components/desktop/home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from '../navbar/navbar/Navbar'
 import Footer from '../../desktop&mobile/footer/Footer'
 import styles from './styles.module.css'
+import Matrix from '../../desktop&mobile/matrix/Matrix'
 
 export default function Home() {
   return (
@@ -10,9 +12,12 @@ export default function Home() {
       <div className={styles.home}>
         <div className={styles.poster}>
           <div className={styles.posterContent}>
-          <div className={styles.motto}>Start a <span className={styles.mottoHighlight}>React</span>ion.</div>
+            <div className={styles.motto}>Start a <span className={styles.mottoHighlight}>React</span>ion.</div>
+            <Link className={styles.category} to="/products?category=hooks">Try our Hooks!</Link>
+            <Link className={styles.category} to="/products?category=redux">Check out Redux!</Link>
+            <Link className={styles.category} to="/products?category=props">Get new Props!</Link>
           </div>
-          
+          <Matrix speed={12} lettersColor="green" letterSize={5}></Matrix>
           <div className={styles.banner}></div>
         </div>
       </div>
