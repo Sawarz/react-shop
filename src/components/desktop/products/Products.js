b0f310a75fa618a65c3b6ef63fe78a3f88d5ea94
unknown <sawarz22@gmail.com>
2022-04-20 09:44:58 +0200

    Add amount of results showing

--- src/components/desktop/products/Products.js
+++ src/components/desktop/products/Products.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import { React, useState } from 'react'
 import styles from './styles.module.css'
 import { useSearchParams } from 'react-router-dom'
 import Product from './productInfo/ProductInfo.js'
 import data from '../../../assets/products/products.json'
 
 export default function Products() {
+  let allResults = 0
+  let showedResults = 0;
+  let defaultResultsPerPage = 30;
+
   const [searchParams] = useSearchParams();
   let productCategory = searchParams.get("category");
-  console.log(productCategory)
   let products = data.products;
   let imgs = [];
+
   for (let i = 0; i < products.length; i++){
     imgs[i] = require(`../../../assets/products/${products[i].src}`);
   }
 
+  products.forEach((product, i) => {
+    if (productCategory === products[i].category || productCategory === null) {
+      allResults++;
+      showedResults = allResults % defaultResultsPerPage;
+    }
+  });
+  
   return (
     <div className={styles.products}>
       <h1 className={styles.productsH1}>Our products</h1>
       <div className={styles.resultsSorting}>
-        <div className={styles.results}>Showing 1-8 of 30 results</div>
+        <div className={styles.results}>Showing 1-{showedResults} of {allResults} results</div>
         <div className={styles.sorting}>No sorting</div>
       </div>
       <div className={styles.productsSorted}>
       {products.map((product, i) => {
-          if(productCategory === products[i].category || productCategory === null)
+        if (productCategory === products[i].category || productCategory === null) {
           return (
             <Product product={products[i]}></Product>
           )
+        }
+        else {
+          return null;
+        }
+          
         })
         }
       </div>
