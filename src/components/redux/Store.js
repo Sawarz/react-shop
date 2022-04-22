ff75cb4e7a28e15cbf6a7210e52251585b504f8c
unknown <sawarz22@gmail.com>
2022-04-22 23:49:13 +0200

    Add state loading from localStorage

--- src/components/redux/Store.js
+++ src/components/redux/Store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shoppingCartSlice from "./shoppingCartSlice";
+import { loadState } from "./localStorage";
+
+const savedState = loadState();
 
 export default configureStore({
     reducer: {
         shoppingCart: shoppingCartSlice
-    }
+    },
+    preloadedState: {shoppingCart: savedState}
 })
