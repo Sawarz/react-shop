7860898cb4c7ce5dea4c835da8815cf74161e2dc
unknown <sawarz22@gmail.com>
2022-04-22 23:48:42 +0200

    Add and remove quantity

--- src/components/redux/shoppingCartSlice.js
+++ src/components/redux/shoppingCartSlice.js
@@ -1,14 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 export const shoppingCartSlice = createSlice({
     name: 'shoppingCart',
     initialState: [],
     reducers: {
         add: (state, action) => {
-            state.push(action.payload); 
+            let arrayExists = false;
+            let arrayIndex = null;
+            for (const [i, productObject] of state.entries()) {
+                if (action.payload.name === productObject.data.name) {
+                    arrayExists = true;
+                    arrayIndex = i;
+                    break;
+                }
+            }
+
+            if (arrayExists) {
+                state[arrayIndex].productCount++;
+            }
+            else {
+                state.push({
+                    data: action.payload,
+                    productCount: 1
+                })
+            }
+            console.log(state[0]);
         },
         remove: (state, action) => {
-           state = "xd"
+            let arrayExists = false;
+            let arrayIndex = null;
+            for (const [i, productObject] of state.entries()) {
+                if (action.payload.name === productObject.data.name) {
+                    arrayExists = true;
+                    arrayIndex = i;
+                    break;
+                }
+            }
+            if (arrayExists) {
+                if (state[arrayIndex].productCount === 1) {
+                    state.splice(arrayIndex, 1);
+                }
+                else {
+                    state[arrayIndex].productCount--;
+                }    
+            }
         }
     }
   })
