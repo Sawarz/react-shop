bcfc5387abf32ca5cd7819be019f4642bd13b548
unknown <sawarz22@gmail.com>
2022-04-22 23:47:47 +0200

    Added quantity and more info

--- src/components/desktop/cart/Cart.js
+++ src/components/desktop/cart/Cart.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { add, remove } from '../../redux/shoppingCartSlice'
 
 export default function Cart() {
+    
+
+    let dispatch = useDispatch();
     let shoppingCart = useSelector((state) => state.shoppingCart)
 
     let imgs = [];
     for (let i = 0; i < shoppingCart.length; i++){
-        imgs[i] = require(`../../../assets/products/${shoppingCart[i].src}`);
+        imgs[i] = require(`../../../assets/products/${shoppingCart[i].data.src}`);
     }
 
     let lowerButtonsContainer = <div className={styles.lowerButtons}>
                                     <Link className={styles.cartLink} to="/">Continue shopping</Link>
                                     <Link className={styles.cartLink} to="/">Payment</Link>
-                                </div>
+    </div>
     
     function renderCart() {
         if(shoppingCart[0]){
@@ -23,8 +27,18 @@ export default function Cart() {
                     return (<div className={styles.cartItem}>
                                 <img className={styles.productImg} src={imgs[i]}></img>
                                 <div className={styles.productInfo}>
-                            <div className={styles.productName}>{item.name}</div>
-                            <div className={styles.productQuantity}>{}</div>
+                                    <div className={styles.productName}>{item.data.name}</div>
+                                    <div className={styles.quantityWrapper}>
+                                    <h2>Quantity:</h2>
+                                    <button className={styles.quantityButton} onClick={() => {
+                                        dispatch(add(item.data))
+                                    }}>+</button>
+                                    <div className={styles.productQuantity}>{item.productCount}</div>
+                                    <button className={styles.quantityButton} onClick={() => {
+                                        dispatch(remove(item.data))
+                                }}>-</button>
+                                <div className={styles.productPrice}>{item.data.price}</div>
+                                    </div>
                                 </div>
                             </div>)
                 })}
