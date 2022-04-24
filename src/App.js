33e728268e98b7b612961013212e3698adc60ed3
unknown <sawarz22@gmail.com>
2022-04-24 23:21:38 +0200

    Routes adjustment and global device state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import { useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setDevice } from './components/redux/deviceSlice';
 import { saveState } from './components/redux/localStorage';
 import DesktopRouteWrapper from './components/desktop/desktop-route-wrapper/DesktopRouteWrapper';
 import Home from './components/desktop/home/Home';
-import Products from './components/desktop/products/Products';
+import Products from './components/desktop&mobile/products/Products';
 import Product from './components/desktop/products/product/Product';
 import Contact from './components/desktop/contact/Contact';
 import AboutUs from './components/desktop/about-us/AboutUs';
 import MobileRouteWrapper from './components/mobile/mobile-route-wrapper/MobileRouteWrapper';
+import MobileContent from './components/mobile/content/MobileContent';
 import MobileContact from './components/mobile/contact/Contact';
-import MobileProducts from './components/mobile/products/Products';
 import MobileAboutUs from './components/mobile/about-us/AboutUs';
+import MobileCart from './components/mobile/cart/Cart';
 import Cart from './components/desktop/cart/Cart';
+import Payment from './components/desktop&mobile/payment/Payment';
 
 function App() {
   const isDesktop = useMediaQuery({ query: '(min-width: 1224px)' })
@@ -23,9 +26,12 @@ function App() {
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
 
   let device;
-
+  
   if(isMobile === true) device = "Mobile";
   if (isDesktop === true) device = "Desktop";
+
+  let dispatch = useDispatch();
+  dispatch(setDevice(device));
   
   let appState = useSelector((state) => state.shoppingCart);
 
@@ -36,13 +42,14 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileContact />} /> : <Home />}></Route>
-        <Route path="/home" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileContact />} /> : <Home />}></Route> 
-        <Route path="/products" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileProducts />} /> : <DesktopRouteWrapper component={<Products />}/>}></Route>
+        <Route path="/" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileContent />} /> : <Home />}></Route>
+        <Route path="/home" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileContent />} /> : <Home />}></Route> 
+        <Route path="/products" element={device === "Mobile" ? <MobileRouteWrapper component={<Products />} /> : <DesktopRouteWrapper component={<Products />}/>}></Route>
         <Route path="/products/:product" element={device === "Mobile" ? <MobileRouteWrapper component={<Product />} /> : <DesktopRouteWrapper component={<Product />}/>}></Route>
         <Route path="/contact" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileContact />} /> : <DesktopRouteWrapper component={<Contact />}/>}></Route>
         <Route path="/about-us" element={device === "Mobile" ? <MobileRouteWrapper component={<MobileAboutUs />} /> : <DesktopRouteWrapper component={<AboutUs />} />}></Route>
-        <Route path="/shopping-cart" element={<Cart></Cart>}></Route>
+        <Route path="/shopping-cart" element={device === "Mobile" ? <MobileCart /> : <Cart />}></Route>
+        <Route path="/payment" element={<Payment />}></Route>
       </Routes>
     </HashRouter>
   );
