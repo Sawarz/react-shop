3671c130f6a9e2aba06768c3c9d452142d04574f
unknown <sawarz22@gmail.com>
2022-08-14 00:22:32 +0200

    More efficient code

--- src/components/desktop/navbar/navbar/Navbar.js
+++ src/components/desktop/navbar/navbar/Navbar.js
@@ -1,52 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import styles from './styles.module.css'
-import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router'
+import { Link, NavLink } from 'react-router-dom'
 import Logo from '../../../desktop&mobile/logo/Logo'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
-  const [productsClassnames, setProductsClassnames] = useState(styles.navbarLink)
-  const [aboutUsClassnames, setAboutUsClassnames] = useState(styles.navbarLink)
-  const [contactClassnames, setContactClassnames] = useState(styles.navbarLink)
-  let location = useLocation();
-
-  const productsRef = useRef(null);
-  const aboutusRef = useRef(null);
-  const contactRef = useRef(null);
-
-  function getLastSegment(url) {
-    return url.substring(url.lastIndexOf("#") + 1);
-  }
-
-  useEffect(() => {
-    switch (location.pathname) {
-      case getLastSegment(productsRef.current.href):
-        setProductsClassnames(`${styles.navbarLink} ${styles.currentLocation}`)
-        setAboutUsClassnames(`${styles.navbarLink}`)
-        setContactClassnames(`${styles.navbarLink}`)
-        break;
-      case getLastSegment(aboutusRef.current.href):
-        setAboutUsClassnames(`${styles.navbarLink} ${styles.currentLocation}`)
-        setProductsClassnames(`${styles.navbarLink}`)
-        setContactClassnames(`${styles.navbarLink}`)
-        break;
-      case getLastSegment(contactRef.current.href):
-        setContactClassnames(`${styles.navbarLink} ${styles.currentLocation}`)
-        setAboutUsClassnames(`${styles.navbarLink}`)
-        setProductsClassnames(`${styles.navbarLink}`)
-        break;
-    }
-    
-  }, [location])
 
   return (
     <div className={styles.navbar}>
       <Logo/>
-      <Link ref={productsRef} className={productsClassnames} to='/products'>Products</Link>
-      <Link ref={aboutusRef} className={aboutUsClassnames} to='/about-us'>About us</Link>
-      <Link ref={contactRef} className={contactClassnames} to='/contact'>Contact</Link>
+      <NavLink  className={({ isActive }) => 
+        isActive ? `${styles.navbarLink} ${styles.currentLocation}` : styles.navbarLink
+      } to='/products'>Products</NavLink>
+      <NavLink  className={({ isActive }) => 
+        isActive ? `${styles.navbarLink} ${styles.currentLocation}` : styles.navbarLink
+      } to='/about-us'>About us</NavLink>
+      <NavLink  className={({ isActive }) => 
+        isActive ? `${styles.navbarLink} ${styles.currentLocation}` : styles.navbarLink
+      } to='/contact'>Contact</NavLink>
       <Link to="/shopping-cart" className={styles.navbarLink}>
         <FontAwesomeIcon icon={faShoppingCart} size="lg"></FontAwesomeIcon>
       </Link>
